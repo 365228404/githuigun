27f13f182a92989dd4a8ef017208fdd9e299c9f8
365228404 <365228404@qq.com>
2020-05-11 22:21:03 +0800

    .

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,8 @@
 </template>
 
 <script>
+
+
 	import Layout from "@/layouts";
 	export default {
 		name: "App",
@@ -20,12 +22,22 @@
 <style lang="scss">
     @import "./style/normalize.scss";
     @import "./style/reset.scss";
+
+
+
+
     @import "./style/common.scss";
     @import "~@/assets/iconfont/iconfont.css";
+
+
+
     #app {
         font-size: $font-size-base;
         color: $color-common;
         font-family: -apple-system, BlinkMacSystemFont, "PingFang SC", "SF UI Text",
         "Helvetica Neue", STHeiti, "Microsoft Yahei", Tahoma, Simsun, sans-serif;
+
+
+
     }
 </style>
